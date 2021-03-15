269ee820696f49440138ec43c2a47ddf6bd97e3f
fajnyCreeper <foltinmatej@gmail.com>
2021-03-15 16:59:42 +0100

    getPattern function

--- api/db.php
+++ api/db.php
@@ -18,3 +18,25 @@ class DatabaseConnector
   {
     $this->connection->close();
   }
+
+  //Get one specific pattern from database
+  public function getPattern($patternId)
+  {
+    $sql = "SELECT * FROM `$this->database`.`$this->table` WHERE pattern='$patternId'";
+    $result = $this->connection->query($sql);
+
+    if ($result->num_rows == 0)
+      return null;
+
+    $result = $result->fetch_assoc();
+    $array = array(
+      "pattern" => $result["pattern"],
+      "100" => $result["100"],
+      "250" => $result["250"],
+      "300" => $result["300"],
+      "400" => $result["400"],
+      "500" => $result["500"],
+      "1000" => $result["1000"]
+    );
+    return $array;
+  }
